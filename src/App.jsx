17b1d2e96696a67c20baf7530f3eddb15f8a85fa
Alvaro <Roekan03@gmail.com>
2023-09-27 16:57:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import { Body } from './common/body/Body'
+import { Header } from './common/header/Header'
+
+function App() {
+
+
+  return (
+    <>
+    <Header />
+    <Body />
+    </>
+  )
+
+}
+
+export default App
